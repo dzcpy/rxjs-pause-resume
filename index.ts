c39a62c9bd96031a076113a2ea1a8e85eedbd0b1
dzcpy <huyifan@vip.sina.com>
2021-02-10 09:19:53 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,17 +1,11 @@
-import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
-import 'rxjs/add/observable/timer';
-import 'rxjs/add/observable/interval';
-import 'rxjs/add/observable/never';
-import 'rxjs/add/operator/timeInterval';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/map';
+import { Observable, Subject, NEVER, timer } from 'rxjs';
+import { switchMap, timeInterval, map } from 'rxjs/operators';
 
 const pauser = new Subject();
 
 pauser
-  .switchMap(paused => paused ? Observable.never() : source())
-  .subscribe(x => console.log(x));
+  .pipe(switchMap((paused) => (paused ? NEVER : source())))
+  .subscribe((x) => console.log(x));
 
 pauser.next(false);
 setTimeout(() => {
@@ -24,11 +18,13 @@ setTimeout(() => {
   }, 3000);
 }, 3000);
 
-function source(): Observable<Date> {
-  return new Observable(observer => {
-    Observable.timer(0, 1000)
-      .timeInterval()
-      .map(() => observer.next(new Date()))
+function source() {
+  return new Observable((observer) => {
+    timer(0, 1000)
+      .pipe(
+        timeInterval(),
+        map(() => observer.next(new Date()))
+      )
       .subscribe();
   });
 }
